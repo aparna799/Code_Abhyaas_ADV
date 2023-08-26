5238bbee7e344342f9d3c249295c24189c795f2f
aparna799 <110480454+aparna799@users.noreply.github.com>
2023-08-26 13:33:29 +0530

    Add files via upload

---
+++ left rotate by k.py
@@ -0,0 +1,20 @@
+def left_rotate(arr,st,end):
+    while(st<end):
+        temp=arr[st]
+        arr[st]=arr[end]
+        arr[end]=temp
+        st=st+1
+        end=end-1
+        
+    return arr
+arr=[5,9,3,9,10]
+n=len(arr)
+k=int(input("enter the positions:"))
+arr=left_rotate(arr,0,k-1);
+arr=left_rotate(arr,k,n-1);
+arr=left_rotate(arr,0,n-1);
+print(arr)
+
+
+        
+
